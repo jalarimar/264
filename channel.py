eb8690810aafe5b855a19ccef3c9ad09475ad9be
Maxwell Clarke <maxeonyx@gmail.com>
2016-09-03 02:08:38 +1200

    tidy up (finished?) channel

--- channel.py
+++ channel.py
@@ -1,57 +1,65 @@
 # hash tag awesome
-import sys
+from sys import argv
 import socket
+from select import select
 import random
+from common import *
 
-IP = '127.0.0.1'
-
+def channel(packet_loss_rate, csin, csout, crin, crout):
+    
+    while (True):
+        readable_sockets, _, _ = select([csin, crin], [], [], 1)
+        for sock in readable_sockets:
+            rcvd, address = sock.recvfrom(PACKET_SIZE)
+            packet = Packet.from_bytes(rcvd)
+            
+            if packet.magicno != 0x497E or random.random() <= packet_loss_rate:
+                print("dropped")
+                continue
+            print("sent")
+            if sock == csin:
+                crout.send(packet.to_bytes())
+            elif sock == crin:
+                csout.send(packet.to_bytes())
 
 def main():
-    # look I used returns m7
+    
+    number_of_arguments = len(argv)
+    if number_of_arguments != 8: # argv[0] is program name
+        abort("Incorrect number of parameters")
+        
+    if number_of_arguments != len(set(argv)):
+        abort("Port numbers not distinct")
+    
+    ports = tuple(int(p) for p in argv[1:7])
+    for port in ports:
+        if (port < 1024) or (port > 64000):
+            abort("Port {} not within valid range 1024-64000".format(port))
+    
+    packet_loss_rate = float(argv[7])
+    if (packet_loss_rate < 0) or (packet_loss_rate >= 1):
+        abort("Incorrect packet loss rate, must be between 0 and 1")
+        
+    channel(packet_loss_rate, *setup_sockets(*ports))
+    
+    
+            
+def setup_sockets(c_s_in_port, c_s_out_port, s_in_port, c_r_in_port, c_r_out_port, r_in_port):
+            
     csin = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     csout = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     crin = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    crout = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    
+    crout = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
-    if len(sys.argv[1:]) != 7: # argv[0] is program name
-        print("Incorrect number of parameters")
-        return
-    if len(sys.argv[1:]) != len(set(sys.argv[1:])):
-        print("Port numbers not distinct")
-        return
-    for channel_port in sys.argv[1:5]:
-        if (channel_port < 1024) or (channel_port > 64000):
-            print("Not within valid range")
-            return
+    csin.bind((HOST_IP, c_s_in_port))
+    csout.bind((HOST_IP, c_s_out_port))  
+    crin.bind((HOST_IP, c_r_in_port))
+    crout.bind((HOST_IP, c_r_out_port))
     
-    csin.bind(IP, int(sys.argv[1]))
-    csout.bind(IP, int(sys.argv[2]))  
-    crin.bind(IP, int(sys.argv[3]))
-    crout.bind(IP, int(sys.argv[4]))
+    csout.connect((HOST_IP, s_in_port))
+    crout.connect((HOST_IP, r_in_port))
     
-    sin_port = int(sys.argv[5]) # port to send to sin from csout
-    csout.connect((IP, sin_port))
-    rin_port = int(sys.argv[6]) # port to send to rin from crout
-    crout.connect((IP, rin_port))
-    
-    packet_loss_rate = float(sys.argv[7])
-    if (packet_loss_rate < 0) or (packet_loss_rate >= 1):
-        print("Incorrect packet loss rate")
-        return
-    
-    while(True):
-        s_readable, s_writable, s_exceptional = select.select([csin], [], [], 1)
-        r_readable, r_writable, r_exceptional = select.select([crin], [], [], 1)
-        if s_readable:
-            rcvd, address = s_readable.recvfrom(512)
-            if rcvd.magicno == 0x497E:
-                if random.random() >= packet_loss_rate:
-                    crout.send(bytes(rcvd))
-        elif r_readable:
-            rcvd, address = r_readable.recvfrom(512)
-            if rcvd.magicno == 0x497E:
-                if random.random() >= packet_loss_rate:
-                    csout.send(bytes(rcvd))        
+    return csin, csout, crin, crout
                     
 
 main()
